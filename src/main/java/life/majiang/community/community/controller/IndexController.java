c272c1a0fc47924042408c48c703e44bc92655a9
lkabb <lkabb1996@foxmail.com>
2019-12-24 16:35:26 +0800

    extract github config from code to properties

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -3,6 +3,9 @@ package life.majiang.community.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Created by lkabb on 2019/12/21
+ */
 @Controller
 public class IndexController {
 
